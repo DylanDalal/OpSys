749e73ece883d6f0a2a1a44dd6fb318c6d1edddd
Dylan Dalal <59746456+DylanDalal@users.noreply.github.com>
2022-01-24 00:21:53 -0500

    Create external_command_execution.c

---
+++ Project1/external_command_execution.c
@@ -0,0 +1,8 @@
+/*
+  "Either the command included a slash or you had to carry out 
+  the $PATH search, you now have a path to the program you are 
+  trying to execute. To put it simply, we have to fork() and 
+  execute the command in the child process using execv()."
+  /*
+  
+#include "main.h"
