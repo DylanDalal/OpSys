419276d1cd5130e47b576699487744698c2a9ac7
Dylan Dalal <59746456+DylanDalal@users.noreply.github.com>
2022-01-24 00:26:44 -0500

    Create cd.c

---
+++ Project1/cd.c
@@ -0,0 +1,10 @@
+/*
+  Implementation of the cd command.
+  Changes the current working directory
+  If no arguments are supplied, change the current working directory to $HOME
+  Signal an error if more than one argument is present
+  Signal an error if the target is not a directory
+  Signal an error if the target does not exist
+  /*
+  
+#include main.h
