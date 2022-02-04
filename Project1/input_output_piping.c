4835fbe8e751ebc0b3e8d7f762181afce829cef5
benbbao123 <95001426+benbbao123@users.noreply.github.com>
2022-02-03 20:32:37 -0500

    entered Redirection

--- Project1/input_output_piping.c
+++ Project1/input_output_piping.c
@@ -1,3 +1,183 @@
+#include <stdio.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/wait.h>
+#include "main.h"
+
+void IO_Redirection(tokenlist* tokens_ptr);
+
+void O_Redirection(tokenlist* tokens_ptr);
+void I_Redirection(tokenlist* tokens_ptr);
+void Both_Redirection(tokenlist* tokens_ptr);
+
+void O_Redirection(tokenlist* tokens_ptr) {
+    token_list* tokens = new_tokenlist();
+    int pos;
+
+    for (int i = 0; i < tokens_ptr->size; i++) {
+        if (tokens_ptr->items[i] == ">") {
+            pos = i;
+        }
+    }
+
+    char* ofile = tokens_ptr->items[pos + 1];
+
+    for (i = 0; i < pos; i++) {
+        add_token(tokens, tokens_ptr->items[i]);
+    }
+
+    char address[300];
+    strcpy(address, "/usr/bin/");
+    strcat(address, tokens_ptr->items[0])
+
+    pid_t pid = fork();
+
+    if (pid == 0) {
+        int stdout = open(ofile, O_CREAT | O_TRUNC | O_RDWR);
+        close(1);
+        dup(stdout);
+        close(stdout);
+
+        execv(tokens_ptr->items[0], tokens->items);
+    }
+
+    else {
+        waitpid(pid, NULL, 0);
+    }
+}
+
+void I_Redirection(tokenlist* tokens_ptr) {
+    token_list* tokens = new_tokenlist();
+    int pos;
+
+    for (int i = 0; i < tokens_ptr->size; i++) {
+        if (tokens_ptr->items[i] == "<") {
+            pos = i;
+        }
+    }
+
+    char* ifile = tokens_ptr->items[pos + 1];
+
+    for (i = 0; i < pos; i++) {
+        add_token(tokens, tokens_ptr->items[i]);
+    }
+
+    pid_t pid = fork();
+
+    if (pid == 0) {
+        int stdin = open(ifile, O_RDONLY);
+        close(0);
+        dup(stdin);
+        close(stdin);
+
+        execv(tokens->items[0], tokens->items);
+    }
+
+    else {
+        waitpid(pid, NULL, 0);
+    }
+}
+
+void Both_Redirection(tokenlist* tokens_ptr) {
+    token_list* tokens = new_tokenlist();
+
+    int in_pos;
+    int out_pos;
+
+    char* ifile = tokens_ptr->items[pos + 1];
+    char* ofile = tokens_ptr->items[pos + 1];
+
+    for (int i = 0; i < tokens_ptr->size; i++) {
+        if (tokens_ptr->items[i] == "<") {
+            in_pos = i;
+        }
+
+        if (tokens_ptr->items[i] == ">") {
+            out_pos = i;
+        }
+    }
+
+    if (in_pos < out_pos) {
+
+        for (i = 0; i < in_pos; i++) {
+            add_token(tokens, tokens_ptr->items[i]);
+        }
+
+        pid_t pid = fork();
+
+        if (pid == 0) {
+            int stdin = open(ifile, O_RDONLY);
+            int stdout = open(ofile, O_CREAT | O_TRUNC | O_RDWR);
+    
+            close(0);
+            dup(stdin);
+            close(stdin);
+
+            close(1);
+            dup(stdout);
+            close(stdout);
+
+            execv(tokens_ptr->items[0], tokens->items);
+        }
+
+        else {
+            waitpid(pid, NULL, 0);
+        }
+    }
+
+    else {
+
+        for (i = 0; i < in_pos; i++) {
+            add_token(tokens, tokens_ptr->items[i]);
+        }
+
+    }
+}
+
+void IO_Redirection(tokenlist* tokens_ptr) {
+
+    bool in;
+    bool out;
+
+    for (int i = 0; i < tokens_ptr->size; i++) {
+        if (tokens_ptr->items[i] == ">" || tokens_ptr->items[i] == "<") {
+
+            if (i == tokens_ptr->size - 1) {
+                printf("-bash: syntax error near unexpected token `newline'");
+                return;
+            }
+
+            else if (i == 0) {
+                printf("command not found");
+                return;
+            }
+
+            if (tokens_ptr->items[i] == ">") {
+                out == true;
+            }
+            
+            if (tokens_ptr->items[i] == "<") {
+                in == true;
+            }
+
+        }
+    }
+
+    if (out && !in) {
+        O_Redirection(tokens_ptr);
+    }
+
+    if (in && !out) {
+        I_Redirection(tokens_ptr);
+    }
+
+    if (in && out) {
+        Both_Redirection(tokens_ptr);
+    }
+}
+
+
 /*
   I/O:
    "CMD > FILE
@@ -17,6 +197,6 @@
      ◦ CMD2 redirects its standard output to CMD3's standard input"
     We're only required to handle two pipes but handling infinite pipes 
     gets us extra credit.
-     /*
+     */
      
 #include "main.h"
